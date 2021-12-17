df9d653a36236a3fcc177508d7297b2f5a856e8c
Antoine <antoine.peyrard@etu.uca.fr>
2021-12-17 15:27:56 +0100

    j'ai dis fini

---
+++ resources/views/Users/index.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="background grid grid-cols-1 m-auto">
+        <div class ="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+            
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Salut à tous
+                </h1>
+                <a 
+                    href="/Games"
+                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="https://cdn.cloudflare.steamstatic.com/store/home/store_home_share.jpg" width="700" alt="">
+        </div>
+
+        <div class="m-auto sm:m-auto text-left w-4/5 block">
+            <h2 class="text-3xl font-extrabold text-gray-600">
+                Vous voulez pleins de Jeux ?
+            </h2>
+            
+            <p class="py-8 text-gray-500 text-s">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+            </p>
+
+            <p class="font-extrabold text-gray-600 text-s pb-9">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
+            </p>
+
+            <a 
+                href="/Games"
+                class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+                Find Out More
+            </a>
+        </div>
+    </div>
+
+    <div class="text-center py-15">
+        <span class="uppercase text-s text-gray-400">
+            Games
+        </span>
+
+        <h2 class="text-4xl font-bold py-10">
+            Recent Games
+        </h2>
+
+        <p class="m-auto w-4/5 text-gray-500">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos temporibus quaerat facere consectetur qui.
+        </p>
+    </div>
+
+    <div class="sm:grid grid-cols-2 w-4/5 m-auto">
+        <div class="flex bg-yellow-700 text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
+                <span class="uppercase text-xs">
+                    Gaming
+                </span>
+
+                <h3 class="text-xl font-bold py-10">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi ex? Nemo hic placeat et?
+                </h3>
+
+                <a 
+                    href=""
+                    class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                    Find Out More
+                </a>
+            </div>
+        </div>
+        <div>
+            <img src="https://cdn03.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_ApexLegends_SeasonBanner_image1600w.jpg" alt="">
+        </div>
+    </div>
+    
+@endsection
